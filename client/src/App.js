488de9c7e46068ad3e8f038c77c40abfe29ed40e
Eunice Kim <112116257+eunykim92@users.noreply.github.com>
2023-12-23 17:03:47 -0800

    added font, changed App.js/reducer to display LandingPage when a user isn't signed in + Signin Signup page. added styling to navbar

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,39 @@ import UserWobbeDrobe from './components/pages/UserWobbedrobe';
 import AddToWobbeDrobe from './components/pages/AddToWobbedrobe';
 import AddOOTD from './components/pages/AddOOTD';
 import LogInSignUpBox from './components/pages/LogInSignUpBox';
+import LandingPage from './components/pages/LandingPage';
 import './styles/App.scss';
 
 function App() {
   const page = useSelector((state) => state.status.page);
+  const user = useSelector((state => state.status.user));
   console.log(page);
+
+  // if no user is logged in and the page is not 'LOGIN' or 'SIGN_UP' show the LANDING_PAGE
+  if (!user && page === 'LANDING_PAGE') {
+    return (
+      <div className='App'>
+        <LandingPage />
+      </div>
+    );
+  }
+  // if the 'sign in' button is clicked on the LandingPage, render the LogInSignUpBox for login
+  if (page === 'LOGIN') { 
+    return (
+      <div className='App'>
+        {page === 'LOGIN' && <LogInSignUpBox isSignUp={false} />}
+        {page === 'SIGN_UP' && <LogInSignUpBox isSignUp={true} />}
+      </div>
+  )
+};
+
   return (
     <div className='App'>
-      <Navbar />
-      {page === 'HOME' && <Home />}
-      {page === 'ADD_TO_WOBBEDROBE' && <AddToWobbeDrobe />}
-      {page === 'VIEW_WOBBEDROBE' && <UserWobbeDrobe />}
-      {page === 'ADD_TO_OOTD' && <AddOOTD />}
-      {page === 'LOGIN' && <LogInSignUpBox isSignUp={false} />}
-      {page === 'SIGN_UP' && <LogInSignUpBox isSignUp={true} />}
+          <Navbar />
+          {page === 'HOME' && <Home />}
+          {page === 'ADD_TO_WOBBEDROBE' && <AddToWobbeDrobe />}
+          {page === 'VIEW_WOBBEDROBE' && <UserWobbeDrobe />}
+          {page === 'ADD_TO_OOTD' && <AddOOTD />}
     </div>
   );
 }
---
+++ client/src/components/pages/LandingPage.jsx
@@ -0,0 +1,21 @@
+// page 4
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { goToPage } from '../../utils/reducers/statusSlice';
+import Navbar from '../Navbar';
+
+export default function Welcome() {
+  const user = useSelector((state) => state.status.user);
+  const page = useSelector((state) => state.status.page);
+  const dispatch = useDispatch();
+
+    return (
+      <div>
+        <Navbar />
+        <p>Hello</p>
+            <div className="landing-page-button">
+              <button onClick={() => dispatch(goToPage('LOGIN'))}>Signin</button>
+            </div>
+      </div>
+    );
+}
