60bac94aeeb9f6847523313886be15db153d38fe
Eunice Kim <112116257+eunykim92@users.noreply.github.com>
2023-12-23 19:28:59 -0800

    trying to push

--- client/src/components/pages/LandingPage.jsx
+++ client/src/components/pages/LandingPage.jsx
@@ -12,7 +12,7 @@ export default function Welcome() {
     return (
       <div>
         <Navbar />
-        <p>Hello my name is eunice hi hi hi</p>
+        <p>Trying to make a commit</p>
             <div className="landing-page-button">
               <button onClick={() => dispatch(goToPage('LOGIN'))}>Signin</button>
             </div>
